d32b6b592933abe909a204f6be10d491443b3ce6
T Corbett <twcorb@gmail.com>
2023-01-22 11:41:49 +0100

    random styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Contact.module.scss'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,7 +9,55 @@ export default function Home() {
         <title>Contact | Muncie Girls</title>
       </Head>
       <main className={styles.main}>
-        <div className='home page' />
+        <div className={`page ${styles.contact}`}>
+
+          <div className={styles.content}>
+            <div className={styles.details}>
+              <div className={styles['one-col']}>
+                <h1>Contact</h1>
+              </div>
+
+              <div className={styles['two-col']}>
+                <h2>General Enquiries</h2>
+                <p>
+                  <Link href="mailto:munciegirls@gmail.com" target="_blank" rel="noopener" title="Email h3 Sarah Maynard">munciegirls@gmail.com</Link></p>
+
+                <h2>Booking</h2>
+                <p>
+                  <span>UK/EU</span>
+                  <Link href="mailto:ed@primarytalent.com" target="_blank" rel="noopener" title="Email Ed Sellers">ed@primarytalent.com</Link></p>
+
+                <p>
+                  <span>US</span>
+                  <Link href="mailto:scott@pinnacleentertainmentinc.com " target="_blank" rel="noopener" title="Email Scott Sokol">scott@pinnacleentertainmentinc.com</Link></p>
+
+                <p>
+                  <span>US</span>
+                  <Link href="mailto:james@pinnacleentertainmentinc.com" target="_blank" rel="noopener" title="Email James Snell">james@pinnacleentertainmentinc.com</Link></p>
+              </div>
+
+              <div className={styles['two-col']}>
+                <h2>Management</h2>
+                <p>
+                  <Link href="mailto:sarah@itsforfriends.com" target="_blank" rel="noopener" title="Email h3 Sarah Maynard">sarah@itsforfriends.com</Link></p>
+
+                <p>
+                  <Link href="mailto:andrew@itsforfriends.com" target="_blank" rel="noopener" title="Email Andrew Kelly">andrew@itsforfriends.com</Link></p>
+
+                <h2>Publicist</h2>
+                <p>
+                  <span>UK/EU</span>
+                  <Link href="mailto:maynard@majortransmission.com" target="_blank" rel="noopener" title="Email Sarah Maynard">maynard@majortransmission.com</Link></p>
+
+                <p>
+                  <span>US</span>
+                  <Link href="mailto:tom@hivemindpr.com" target="_blank" rel="noopener" title="Email Sarah Maynard">tom@hivemindpr.com</Link></p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.bg} />
+        </div>
       </main>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,7 +10,38 @@ export default function Home() {
         <title>Muncie Girls</title>
       </Head>
       <main className={styles.main}>
-        <div className='home page' />
+        <div className={`page ${styles.home}`}>
+          <h1>Fixed Ideals</h1>
+
+          <div className={styles.bg} />
+
+          <div className={styles.album}>
+            <div className={styles.image}>
+              <Image
+                alt="Fixed Ideals album cover"
+                src="/images/fixed_ideals_cover.jpg"
+                width="1464"
+                height="1464"
+              />
+            </div>
+          </div>
+
+          <div className="cta">
+            <a className={styles.preorder}
+              href="https://munciegirls.bandcamp.com/album/fixed-ideals-3"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Order now">
+              Order now
+            </a>
+
+            <Link
+              className={styles.second}
+              href="/tour">
+              Buy tickets
+            </Link>
+          </div>
+        </div>
       </main>
     </>
   )
